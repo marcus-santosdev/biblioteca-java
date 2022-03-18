1c30f2ec949cf3ef4194db8760ae7506d93a7821
Marcus Santos <mvsantosdev@gmail.com>
2022-03-18 04:06:10 -0300

    enviando arquivos do projeto

---
+++ src/br/com/fiap/bytebank/TesteBiblioteca.java
@@ -0,0 +1,18 @@
+package br.com.fiap.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new ContaCorrente(123, 321);
+		
+		conta.deposita(200);
+		
+		System.out.println(conta.getSaldo());
+		
+	}
+
+}
